277112da2cea71254ec684c17a724b2012de8781
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-18 10:53:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,21 @@
 class Song
   attr_accessor :name, :artist, :genre
-  @@artists = []
-  @@
+  @@SONG_HASH = {
+    count: 0
+    artists: []
+    genres: []
+  }
   
-  def initialize (name, artist, genre)
+  def initialize (name, artist_name, genre_name)
     self.name = name
-    self.artist = artist
-    self.genre = genre
+    self.artist = artist_name
+    self.genre = genre_name
+    @@SONG_HASH[count] += 1
+    @@SONG_HASH[artists] << artist_name
+    @@SONG_HASH[genres] << genre_name
   end
+  
+  
   
   
 end
