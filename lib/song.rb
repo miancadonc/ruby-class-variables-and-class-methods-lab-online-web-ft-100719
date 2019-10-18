8bb54bc38ad847943555d1dbfd65421eb3802db1
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-18 10:48:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@artists = []
+  @@
+  
+  def initialize (name, artist, genre)
+    self.name = name
+    self.artist = artist
+    self.genre = genre
+  end
+  
+  
+end
