4f3e87eded162e07ac4450cf2a8358f0737b0996
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-18 11:27:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,11 +28,17 @@ class Song
   end
   
   def self.genre_count
-    
+    @@genres.each do |genre|
+       !!@@genre_count[genre] ? @@genre_count[genre] +=1 : @@genre_count[genre] = 1
+    end
+    @@genre_count
   end
   
   def self.artist_count
-    
+    @@artists.each do |artist|
+      !!@@artist_count[artist] ? @@artist_count[artist] +=1 : @@artist_count[artist] = 1
+    end
+    @@artist_count
   end
   
 end
